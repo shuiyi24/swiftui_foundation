eb0a6b6793b87d3ef708e0549eb3097a0a424dc7
1794744155@qq.com <1794744155@qq.com>
2023-12-08 14:36:22 +0800

    Initial Commit

---
+++ swift_foundation/swift_foundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_foundationApp.swift
+//  swift_foundation
+//
+//  Created by 123 on 2023/12/8.
+//
+
+import SwiftUI
+
+@main
+struct swift_foundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
